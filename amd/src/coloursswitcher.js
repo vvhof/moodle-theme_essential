23af2c24a545569b8caf67c0bcf64f9d66444a48
Gareth J Barnard <gjbarnard@gmail.com>
2016-03-21 20:47:35 +0000

    More in progress.

--- amd/src/coloursswitcher.js
+++ amd/src/coloursswitcher.js
@@ -45,13 +45,9 @@ define(['jquery', 'core/log'], function($, log) {
                 // Update the current colour.
                 event.data.us.scheme = event.data.scheme;
                 // Store the users selection (Uses AJAX to save to the database).
+                // Core YUI function, so only need to replace if core changes.
                 M.util.set_user_preference('theme_essential_colours', event.data.us.scheme);
                 log.debug('Colour switcher setScheme(after) our scheme: ' + event.data.us.scheme);
-            },
-            blip: function(thing) {
-                log.debug('Thing: ' + thing);
-                log.debug('Element: ' + this.$element.attr('class'));
-                log.debug('Scheme: ' + this.scheme);
             }
         };
 
@@ -73,82 +69,11 @@ define(['jquery', 'core/log'], function($, log) {
     return {
         init: function(data) {
             $(document).ready(function($) {
-                var cs = $(document.body).ColoursSwitcher(data);
-                cs.colourswitcher.blip('Hullo!');
-                return cs;
+                log.debug('jQuery version: ' + $.fn.jquery);
+                return $(document.body).ColoursSwitcher(data);
             });
             log.debug('Essential Colour Switcher AMD init');
         }
     }
 });
 /* jshint ignore:end */
-
-/*
-YUI.add('moodle-theme_essential-coloursswitcher', function (Y) {
-
-// Available color schemes.
-    var SCHEMES = ['default', 'alternative1', 'alternative2', 'alternative3', 'alternative4'];
-
-    /**
-     * Essential theme colours switcher class.
-     * Initialise this class by calling M.theme_essential.init
-     */ /*
-    var ColoursSwitcher = function () {
-        ColoursSwitcher.superclass.constructor.apply(this, arguments);
-    };
-    ColoursSwitcher.prototype = {
-        /**
-         * Constructor for this class
-         * @param {object} config
-         */ /*
-        initializer: function (config) {
-            var i, s;
-            /* Attach events to the links to change colours scheme so we can do it with
-               JavaScript without refreshing the page. */ /*
-            Y.log('Colour switcher on: ' + config.div);
-            for (i in SCHEMES) {
-                s = SCHEMES[i];
-                // Check if this is the current colour.
-                if (Y.one(document.body).hasClass('essential-colours-' + s)) {
-                    this.set('scheme', s);
-                }
-                Y.all(config.div + ' .colours-' + s).each(function (node) {
-                    node.ancestor().on('click', this.setScheme, this, s);
-                }, this);
-            }
-        },
-        /**
-         * Sets the colour being used for the essential theme
-         * @param {Y.Event} e The event that fired
-         * @param {string} colours The new colours scheme
-         */ /*
-        setScheme: function (e, scheme) {
-            // Prevent the event from refreshing the page.
-            e.preventDefault();
-            // Switch over the CSS classes on the body.
-            var prefix = 'essential-colours-';
-            Y.one(document.body).replaceClass(prefix + this.get('scheme'), prefix + scheme);
-            // Update the current colour.
-            this.set('scheme', scheme);
-            // Store the users selection (Uses AJAX to save to the database).
-            M.util.set_user_preference('theme_essential_colours', scheme);
-        }
-    };
-// Make the colours switcher a fully fledged YUI module.
-    Y.extend(ColoursSwitcher, Y.Base, ColoursSwitcher.prototype, {
-        NAME: 'Essential theme colours scheme switcher',
-        ATTRS: {
-            scheme: {
-                value: 'default'
-            }
-        }
-    });
-// Our Essential theme namespace
-    M.theme_essential = M.theme_essential || {};
-// Initialisation function for the colours scheme switcher
-    M.theme_essential.initColoursSwitcher = function (cfg) {
-        return new ColoursSwitcher(cfg);
-    }
-
-}, '@VERSION@', {requires: ['base', 'node']});
-*/
